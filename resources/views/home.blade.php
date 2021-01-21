526ed3e954acf344df229182b224e17e4df88289
alessio <alessio.cirillo.1995@gmail.com>
2021-01-21 16:36:15 +0100

    creazione DB, model, migration, controller e route::resource

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('content')    
+    <div class="container mb-5">
+        <h1>school homepage here</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos modi ducimus eveniet non praesentium quae quo quibusdam, doloremque neque amet ipsam rem molestiae dolorem mollitia saepe pariatur, vitae facere fugit!</p>
+    </div>
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer>
+    <p class="text-center">school site powered by class 19</p>
+</footer>
